cb8fbafdb4dbe0c37aceaab06db997d1fc2dc08e
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2022-10-25 23:22:59 +0200

    Add files via upload

---
+++ uloha 1.py
@@ -0,0 +1,11 @@
+def ul1(n: int, word: str):
+    if n >= len(word):
+        return print('zly index')
+    else:
+        return print(word[n])
+
+
+uloha1(6, 'kamikadze')
+uloha1(3, 'kuk')
+uloha1(0, ' ')
+uloha1(0, '')
