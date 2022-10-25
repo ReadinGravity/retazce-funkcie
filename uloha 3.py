285fe62627b967569ec850e81e5d151801e26683
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2022-10-25 23:23:39 +0200

    Add files via upload

---
+++ uloha 3.py
@@ -0,0 +1,16 @@
+def ul3(ret):
+    samo = 'euioa'
+    a = True
+    for i in ret:
+        if i in samo:
+            a = True
+        else:
+            a = False
+            break
+    return print(a)
+
+
+uloha3("kukucka")
+uloha3("aaaaaauuu")
+uloha3("aeioux")
+uloha3("o")
